78a89c06d49d21e9b9f30089816a548876ddc8e5
Chen Junda <smallda@outlook.com>
2016-10-30 13:04:09 +0800

    separated settings from the main py updated descriptions for login status

--- login.py
+++ login.py
@@ -1,15 +1,14 @@
 import urllib.request
 import urllib.parse
 import json
+import settings
 from datetime import datetime
 
-#使用前修改这里！！双引号不要删掉！！
-username="用户名"
-password="密码"
-#使用前改这里！！！改双引号里的东西！！！！
+username=settings.username
+password=settings.password
 
 if (username=="用户名") or (password=="密码"):
-    print("先右键编辑这个文件设置用户名和密码。")
+    print("先编辑settings.py文件设置用户名和密码。")
     exit()
 
 url="http://p.nju.edu.cn/portal_io/login"
@@ -25,8 +24,10 @@ result=json.loads(page)
 result_code=int(result['reply_code'])
 
 if result_code in (1,6):
-    print("登陆成功！\n")
-
+    if result_code==1:
+        print("登录成功！\n")
+    else:
+        print("已经登录！\n")
     name=result['userinfo']['fullname']
     balance=result['userinfo']['balance']
     time=datetime.fromtimestamp(int(result['userinfo']['acctstarttime']))
@@ -35,4 +36,4 @@ if result_code in (1,6):
     print("余额：{0}".format(balance))
     
 if result_code==3:
-    print("密码错误！右键编辑这个文件修改密码。")
+    print("密码错误！编辑settings.py文件设置用户名和密码。")
---
+++ settings.py
@@ -0,0 +1,8 @@
+username="用户名"
+password=i"密码"
+
+#下面的不要管
+if __name__=="__main__":
+	print(username)
+	print(password)
+
